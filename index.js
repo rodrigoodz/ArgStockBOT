4ccb5611ef10f91db56f504bc38d289724f2d96d
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2020-08-17 12:07:25 -0300

    Se agrego una logica sencilla para tener registro de los grupos a los que el bot ha sido agregado (utilizando un arreglo de objetos)

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const config = require("./config/config.json");
 const fs = require("fs");
 
 // node-telegram-bot-api
-
 const token = config.token;
 const bot = new TelegramBot(token, { polling: true });
 
 //db
-// let chats = [];
-// console.log(chats);
+let chats = [];
+console.log(chats);
 
 //bot_id
 const bot_id = config.bot_id;
@@ -30,17 +29,26 @@ bot.onText(/\/start/, (msg) => {
 
 bot.on("new_chat_members", (msg) => {
   //verifico si el id del "nuevo miembro" coincide con el del bot
-  const { id } = msg.new_chat_member;
-  if (id === bot_id) {
-    console.log("Te agregaron al grupo -> " + msg.chat.title);
-    // console.log(msg);
+  const { id: userID } = msg.new_chat_member;
+  const { id: GroupID, title: GroupTITLE } = msg.chat;
+  if (userID === bot_id) {
+    console.log("Te agregaron al grupo -> " + GroupTITLE);
+    //si fue agregado el bot, guardo el id del grupo
+    bot.sendMessage(GroupID, "Gracias por la invitacion");
+    chats.push({ title: GroupTITLE, id: GroupID });
+    console.log(chats);
   }
 });
 
 bot.on("left_chat_member", (msg) => {
   //verifico si el id del "miembro que se ha ido" coincide con el del bot
   const { id } = msg.left_chat_member;
+  const { id: GroupID, title: GroupTITLE } = msg.chat;
   if (id === bot_id) {
-    console.log("Te fuiste del grupo -> " + msg.chat.title);
+    console.log("Te fuiste del grupo -> " + GroupTITLE);
   }
+  chats = chats.filter((chat) => {
+    return chat.id !== GroupID;
+  });
+  console.log(chats);
 });
