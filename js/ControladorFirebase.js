616b9917ab33c639c98667465ffa01fe06d365a3
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2020-08-28 00:48:26 -0300

    agregadas process.env para credenciales Firebase

--- js/ControladorFirebase.js
+++ js/ControladorFirebase.js
@@ -2,10 +2,22 @@
 const admin = require("firebase-admin");
 
 //firebase
-const serviceAccount = require("../arg-stock-bot-firebase-adminsdk-we592-92aa118e6b.json");
+// const serviceAccount = require("../arg-stock-bot-firebase-adminsdk-we592-92aa118e6b.json");
 const { database } = require("firebase-admin");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
   databaseURL: "https://arg-stock-bot.firebaseio.com",
 });
 const db = admin.database();
