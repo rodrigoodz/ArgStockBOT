7420006fee3754c43c3dc44b97974c2904644271
Rodrigo Odz <rodrigoodz_6@hotmail.com>
2020-08-28 17:53:54 -0300

    mensajes de apertura/cierre borrados luego de 1 min

--- js/InformeAperturaCierre.js
+++ js/InformeAperturaCierre.js
@@ -11,21 +11,49 @@ const informeApertura = async (apertura) => {
   if (chats.length > 0) {
     if (apertura === 1) {
       chats.forEach((chat) => {
-        bot.sendMessage(
-          chat.id,
-          "<b>El mercado abre en aproximadamente 5 minutos!</b>",
-          { parse_mode: "HTML" }
-        );
-        bot.sendPhoto(chat.id, "https://unsplash.com/photos/N__BnvQ_w18");
+        bot
+          .sendMessage(
+            chat.id,
+            "<b>El mercado abre en aproximadamente 5 minutos!</b>",
+            { parse_mode: "HTML" }
+          )
+          .then((mensaje) => {
+            //despues de 2 minutos borro el mensaje de todos los grupos
+            setTimeout(() => {
+              bot.deleteMessage(mensaje.chat.id, mensaje.message_id);
+            }, 60000);
+          });
+        bot
+          .sendPhoto(chat.id, "https://unsplash.com/photos/N__BnvQ_w18")
+          .then((mensaje) => {
+            //despues de 2 minutos borro el mensaje de todos los grupos
+            setTimeout(() => {
+              bot.deleteMessage(mensaje.chat.id, mensaje.message_id);
+            }, 60000);
+          });
       });
     } else {
       chats.forEach((chat) => {
-        bot.sendMessage(
-          chat.id,
-          "<b>El mercado cierra en aproximadamente 5 minutos!</b>",
-          { parse_mode: "HTML" }
-        );
-        bot.sendPhoto(chat.id, "https://unsplash.com/photos/ZzOa5G8hSPI");
+        bot
+          .sendMessage(
+            chat.id,
+            "<b>El mercado cierra en aproximadamente 5 minutos!</b>",
+            { parse_mode: "HTML" }
+          )
+          .then((mensaje) => {
+            //despues de 2 minutos borro el mensaje de todos los grupos
+            setTimeout(() => {
+              bot.deleteMessage(mensaje.chat.id, mensaje.message_id);
+            }, 60000);
+          });
+        bot
+          .sendPhoto(chat.id, "https://unsplash.com/photos/ZzOa5G8hSPI")
+          .then((mensaje) => {
+            //despues de 2 minutos borro el mensaje de todos los grupos
+            setTimeout(() => {
+              bot.deleteMessage(mensaje.chat.id, mensaje.message_id);
+            }, 60000);
+          });
       });
     }
   }
