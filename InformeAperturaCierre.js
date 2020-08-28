b8a57152b9832ed03587d32ce470f45d31acec0e
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2020-08-27 23:03:17 -0300

    informeAperturaCierre mediante Firebase

--- InformeAperturaCierre.js
+++ InformeAperturaCierre.js
@@ -1,16 +1,16 @@
 // Imports
 const TelegramBot = require("node-telegram-bot-api");
+const { getDBFirebase } = require("./ControladorFirebase");
 
 // node-telegram-bot-api
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token);
 
-const informeApertura = (apertura) => {
-  const chats = getListado();
+const informeApertura = async (apertura) => {
+  const chats = await getDBFirebase();
   if (chats.length > 0) {
     chats.forEach((grupo) => {
       if (apertura === 1) {
-        console.log(chats);
         chats.forEach((chat) => {
           bot.sendMessage(
             chat.id,
@@ -18,11 +18,6 @@ const informeApertura = (apertura) => {
             { parse_mode: "HTML" }
           );
         });
-        // bot.sendMessage(
-        //   grupo.id,
-        //   "<b>El mercado abre en aproximadamente 5 minutos!</b>",
-        //   { parse_mode: "HTML" }
-        // );
       } else {
         bot.sendMessage(
           grupo.id,
